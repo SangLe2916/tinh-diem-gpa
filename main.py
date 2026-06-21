6354832a98beb24ae92c7d39a5d79fd638af26a5
PhanQuan26 <phanvanquann26@gmail.com>
2026-06-21 12:41:57 +0700

    feat: dev A tinh gpa theo tuyen tinh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #main goo tren nhanh main
 def tinh_diem_gpa(diem_so):
-  #TODO: can cap nhat logic quy doi sang he 4
-  return 0.0
-print("Diem GPA he 4 la:", tinh_diem_gpa(8.5))
+  #cong thuc tinh don gian cua dev A
+  return round((diem_so/10)*4, 2)
